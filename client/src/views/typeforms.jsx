c832a51dcd67701e969be06d6c45f67cdb2d3177
Dameon Green <dameongreen@gmail.com>
2020-05-26 10:55:03 -0400

    initial

---
+++ client/src/views/typeforms.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+ 
+class Typeforms extends React.Component {
+  render() {
+    return <ReactTypeformEmbed url="https://demo.typeform.com/to/njdbt5" />;
+  }
+}
+
+export default Typeforms;
